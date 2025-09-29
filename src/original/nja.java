8b0f3e45a206f5c46d3d284dec323c019fe678f7
SunnyJasmine <jhsigmajh@gmail.com>
2025-09-29 19:37:57 +0300

    Pirmais

---
+++ src/original/nja.java
@@ -0,0 +1,30 @@
+package original;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class nja {
+	
+
+	static void mestKaulinu(int reizes) {
+		int skaitlis;
+		Random rand = new Random();
+		
+		for(int i = 1; i <= reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis);
+		}
+	}
+	public static void main(String[] args) {
+		int reizes;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Esiet sveicināti manā programmā");
+		System.out.println("Ievadiet cik reizes mest kauliņu");
+		reizes = scan.nextInt();
+		
+		mestKaulinu(reizes);
+		scan.close();
+	}
+	
+
+}
